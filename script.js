a0435dcc8fd42d3a6e965ed3618443d2b75449f4
soufianekkk <75423824+soufianekkk@users.noreply.github.com>
2020-12-23 16:38:54 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+let modenoir = false
+document.querySelector("#modedark").addEventListener("click", function () {
+    if (modenoir == false) {
+        document.querySelector("body").classList.toggle("darkmode");
+    } else {
+        document.querySelector("body");
+    }
+}) 
+
